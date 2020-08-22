7296ae098c96342162a6d00a86a6c861c3c97ef9
caickmarques <caick_marques@hotmail.com>
2020-08-22 18:54:13 -0300

    Criado front end da pagina de resultados

---
+++ functions.js
@@ -0,0 +1,8 @@
+const voltar = () => {
+    event.preventDefault();
+    window.location.href = 'http://127.0.0.1:5500/pesquisar_equipamento.html';
+}
+const pesquisar = () => {
+    event.preventDefault();
+    window.location.href = 'http://127.0.0.1:5500/resultado_pesquisa.html';
+}
